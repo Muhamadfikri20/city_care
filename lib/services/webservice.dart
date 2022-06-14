3b3706cf90beffdc1fee004c6769a07f712e708b
Muhamadfikri20 <muhamadfikri9602@gmail.com>
2022-06-14 15:46:52 +0700

    Lecture_49_ImplementingWebserviceMethod_to_GetAllIncidents

--- lib/services/webservice.dart
+++ lib/services/webservice.dart
@@ -7,4 +7,16 @@ class Webservice {
 
     await Dio().post(url, data: {"title": incident.title, "description": incident.description}, options: Options(contentType: "application/x-www-form-urlencoded"));
   }
+
+  Future<List<Incident>> getAllIncidents() async {
+    final url = "https://vast-savannah-75068.herokuapp.com/incidents";
+
+    final response = await Dio().get(url);
+    if (response.statusCode == 200) {
+      final Iterable json = response.data;
+      return json.map((incident) => Incident.fromJson(incident)).toList();
+    } else {
+      throw Exception("Unable to get incidents");
+    }
+  }
 }
