e7deab65ac58c05dd765b64c8dd9bec802ffb25f
Muhamadfikri20 <muhamadfikri9602@gmail.com>
2022-06-14 16:03:03 +0700

    Lecture_50_Updating_IncidentListViewModel_to_FetchAllIncidents

--- lib/view_models/incident_list_view_model.dart
+++ lib/view_models/incident_list_view_model.dart
@@ -1,1 +1,18 @@
-class IncidentListViewModel {}
+import 'package:city_care/services/webservice.dart';
+import 'package:city_care/view_models/incident_view_model.dart';
+import 'package:flutter/material.dart';
+
+enum Status { loading, empty, success }
+
+class IncidentListViewModel extends ChangeNotifier {
+  List<IncidentViewModel> incidents = <IncidentViewModel>[];
+  Status status = Status.empty;
+
+  Future<void> getAllIncidents() async {
+    status = Status.loading;
+    final results = await Webservice().getAllIncidents();
+    incidents = results.map((incident) => IncidentViewModel(incident: incident)).toList();
+    status = incidents.isEmpty ? Status.empty : Status.success;
+    notifyListeners();
+  }
+}
